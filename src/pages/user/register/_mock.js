8f47d9616b75c668ddc53c3dc61e9a7f58e6f427
hana <jdxtxbb@163.com>
2020-05-02 22:24:26 +0800

    Layout

--- src/pages/user/register/_mock.js
+++ src/pages/user/register/_mock.js
@@ -3,7 +3,7 @@ export default {
   'POST  /api/register': (_, res) => {
     res.send({
       success: false,
-      msg: '就这?',
+      msg: '就这?我裤子都不东一下的',
       code: 'JZ',
       data: {
         iii: 'i18n',
